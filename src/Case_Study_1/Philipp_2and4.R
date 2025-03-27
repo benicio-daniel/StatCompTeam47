dbd72c50aca1ead4fb6c6a850ec3752d70d64aba
PhilippS01 <152029240+PhilippS01@users.noreply.github.com>
2025-03-27 12:21:29 +0100

    A2 Anfang

--- src/Case_Study_1/Philipp_2and4.R
+++ src/Case_Study_1/Philipp_2and4.R
@@ -1,3 +1,15 @@
 # Aufgabe 2
+#a)
+gamma <- function(n) {
+    factorial(n-1)
+}
+
+gamma_function <- function(n) {
+  numerator <- gamma((n - 1) / 2)
+  denominator <- gamma(1 / 2) * gamma((n - 2) / 2)
+  result <- numerator / denominator
+  return(result)
+}
+
 
 # Aufgabe 4
